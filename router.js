fb96d283ae228c0ce30f0d8f48e0af1b6826562d
Michael Sakyiama <ms853@student.le.ac.uk>
2019-07-20 05:10:57 +0100

    added start script to package.json and added a test to router config

--- router.js
+++ router.js
@@ -3,9 +3,11 @@ const Authentication = require('./controllers/AuthenticationController');
 //export module functions.
 //typical get request function. 
 module.exports = function(app) {
-    // app.get('/', function(req, res, next) {
-    //     res.send(['Mike','Spike','Like']);
-    // });
+	//for debug
+     app.get('/', function(req, res, next) {
+         res.send(['Mike','Spike','Like']);
+     });
+	//Router configurations
     app.post('/signup', Authentication.signup);
     app.post('/login', Authentication.login);
     app.post('/reqOneTimePassword', Authentication.reqOneTimePassword);
