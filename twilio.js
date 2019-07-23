8ec490368d7a5dd4683bca1e63f6b221ef2b462a
Michael Sakyiama <ms853@student.le.ac.uk>
2019-07-23 04:07:32 +0100

    deleting twilio and service account file

--- twilio.js
+++
@@ -1,8 +0,0 @@
-//set up for the twilio api. 
-//This api will be used to send sms messages, providing users with their code for two factor authentication.
-const twilio = require('twilio');
-
-const accountSid = 'AC92acd5ec2a489070e91deb0ff193cc79';
-const authToken = '067e11d7bf2e1d1d55f25debe22cf270';
-//Twilio purchased number - +441456256016
-module.exports = new twilio(accountSid, authToken);
